bbc7250841856e06c3772a50d31e7cf0bfb11847
Alejandra Duran <ad2002@princeton.edu>
2023-10-25 10:20:24 -0400

    final linked list

--- symtable.h
+++ symtable.h
@@ -1,4 +1,4 @@
-/* INTERFACE (HEADER) FILE */
+/* ---------- Header file for SymbolTable implementation ------------ */
 
 #include <stdlib.h>
 #include <assert.h>
@@ -6,40 +6,106 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* THESE ARE ADTs */
-
+/* Encapsulated ADTs*/
 struct SymTable;
 typedef struct SymTable *SymTable_T;
 
-/* THESE ARE FUNCTIONS - NEED TO COMMENT */
 
-/* COMMENT */
+/* Initialize a SymTable                                                                                                                                                                                                                                                                                                                                                                                                    
+------------------------                                                                                                                                                                       
+return: a new SymTable object with no bindings, or NULL if 
+        insufficient memory is available                             
+*/
 SymTable_T SymTable_new(void);
 
-/* COMMENT */
+
+/* Free all memory occupied by SymTable and its members   
+------------------------    
+param: oSymTable: pointer to SymTable where binding is to be inserted
+                                                                                                                                                                                      
+return: (void) 
+*/
 void SymTable_free(SymTable_T oSymTable);
 
-/* COMMENT */
+
+/* Get length of SymTable       
+------------------------    
+oSymTable: pointer to SymTable where binding is to be inserted
+                                                                                                                                                                                      
+return: number of bindings (size_t) 
+*/
 size_t SymTable_getLength(SymTable_T oSymTable);
 
-/* COMMENT */
-int SymTable_put(SymTable_T oSymTable,
-   const char *pcKey, const void *pvValue);
 
-/* COMMENT */
-void *SymTable_replace(SymTable_T oSymTable,
-   const char *pcKey, const void *pvValue);
+/* Insert a new binding in SymTable   
+Fails if SymTable already contains pcKey or if insufficient 
+memory is available           
+------------------------    
+param: oSymTable: pointer to SymTable where binding is to be inserted
+param: pcKey: literal key of binding to be inserted
+param: pvValue: value (any type) of binding to be inserted
+                                                                                                                                                                                      
+return: 1 (TRUE) if new binding with pcKey and pcValue is added,
+0 (FALSE) otherwise 
+*/
+int SymTable_put(SymTable_T oSymTable, const char *pcKey, 
+    const void *pvValue);
 
-/* COMMENT */
+
+/* Replace a binding's value in SymTable
+No changes if SymTable has no binding with key pcKey
+------------------------    
+param: oSymTable: pointer to SymTable where binding is to be inserted
+param: pcKey: literal key of binding to be accessed
+param: pvValue: value (any type) of binding to be replaced
+                                                                                                                                                                                      
+return: (void)
+*/
+void *SymTable_replace(SymTable_T oSymTable, const char *pcKey, 
+    const void *pvValue);
+
+
+/* Check if SymTable contains a given binding
+------------------------    
+param: oSymTable: pointer to SymTable where binding is to be inserted
+param: pcKey: literal key of target binding
+                                                                                                                                                                                     
+return: 1 (TRUE) if binding is contained, 0 (FALSE) otherwise
+*/
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
 
-/* COMMENT */
+
+/* Get the value of a binding in SymTable at a given key
+------------------------ 
+param: oSymTable: pointer to SymTable
+param: pcKey: literal key of binding with target value
+
+return: (void) void pointer to value of the target binding, NULL if 
+        no such binding exists 
+*/
 void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
 
-/* COMMENT */
+
+/* Remove binding at a given key from SymTable
+Get the value of a binding in SymTable at a given key
+------------------------ 
+param: oSymTable: pointer to SymTable
+param: pcKey: literal key of binding to be removed
+
+return: (void) void pointer to value of the removed binding, NULL if 
+        no such binding exists 
+*/
 void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
 
-/* COMMENT */
+
+/* Apply given function to all bindings in SymTable
+------------------------ 
+param: oSymTable: pointer to SymTable
+param: pfApply: pointer to function to be applied
+param: pvExtra: extra parameter passed to pfApply
+
+return: (void) 
+*/
 void SymTable_map(SymTable_T oSymTable,
    void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
    const void *pvExtra);
--- symtablelist.c
+++ symtablelist.c
@@ -1,53 +1,42 @@
-/* COMMENT SOMETHING HERE */
+/* ----------- Symbol Table linked list implementation -------------- */
+ /* QUESTION: clearly this first allocation in the heap is in the
+    implementation but why is this even necessary */
+     /* QUESTION: can you do this */
 
 #include "symtable.h"
 
-/* you CANNOT dereference a void pointer for assignment,
-just in case of printing 
-SymTable_T is a pointer to SymTable given by h*/
-
-/* building the data types (ADTs) PERHAPS MORE GENERAL? */
-/* TO DO sizeof(struct Node) will be 4 by padding? 8 the longest?*/
+/* Each item is stored in a Node. Nodes are linked. */
 struct Node {
-    const char *key;
-    const void *item;    /* the item (any type )*/
-    struct Node *next;
+    const char *key;        /* unique key (string)*/
+    const void *item;       /* the item (any type )*/
+    struct Node *next;      /* address of next node*/
 };
 
-/* when allocating the memory*/
+/* A SymTable is a struct that points to the first Node */
 struct SymTable {
     struct Node *first;
-    size_t len;
+    size_t len;             /* keep track of list length */
 };
 
-/* THESE ARE FUNCTIONS in .h*/
-/* QUESTION: can I keep using SymTable_T*/
 
-/* return a new SymTable object that contains no bindings, or NULL if insufficient memory is available.*/
-/* treating as a stack */
 SymTable_T SymTable_new(void) {
-    /* declare pointer to a symtable */
+
     SymTable_T oTable;
-    /* allocate memory - GIVE THE TYPE NOT POINTER
-    bc pointers are always 8 bytes on 64-bit
-    QUESTION: clearly this first allocation in the heap is in the
-    implementation but why is this even necessary */
+    /* Allocate memory for head of list */
     oTable = (SymTable_T)malloc(sizeof(struct SymTable));
-    /* if insufficient space */
-    if (oTable == NULL) {
+
+    if (oTable == NULL) {      /* insufficient space*/
         return NULL;
     }
-    /* set the first node pointer to NULL (empty) */
-    oTable->first = NULL;
 
+    oTable->first = NULL;      /* initialize head of list to NULL */
     return oTable;
 }
 
-/* free all memory occupied by oSymTable. */
-/* treating as a stack (will go through all nodes )*/
-void SymTable_free(SymTable_T oSymTable) {
-    struct Node *curr, *next;
 
+void SymTable_free(SymTable_T oSymTable) {
+
+    struct Node *curr, *next;
     assert (oSymTable != NULL);
 
     for (curr = oSymTable->first; curr != NULL; curr = next) {
@@ -57,31 +46,25 @@ void SymTable_free(SymTable_T oSymTable) {
         free(curr);
     }
 
-    /* free memory of SymTable itself, left with 1 struct pointer*/
+    /* free memory of SymTable head itself */
     free(oSymTable);
-
+    /* reset length */
     (*oSymTable).len = 0;
 }
 
-/* return the number of bindings in oSymTable */
+
 size_t SymTable_getLength(SymTable_T oSymTable) {
 
     assert (oSymTable != NULL);
-
     return (*oSymTable).len;
 }
 
-/* If oSymTable does not contain a binding with key pcKey, then SymTable_put 
-must add a new binding to oSymTable consisting of key pcKey and value 
-pvValue and return 1 (TRUE). Otherwise the function must leave oSymTable 
-unchanged and return 0 (FALSE). If insufficient memory is available, 
-then the function must leave oSymTable unchanged and return 0 (FALSE)
-QUESTION: i can't assert the void pointer for null right */
+
 int SymTable_put(SymTable_T oSymTable,
-   const char *pcKey, const void *pvValue) {
+                 const char *pcKey, const void *pvValue) {
 
     struct Node *new;
-    const char *keyCopy;    /* POT BUG: should this be const? */
+    const char *keyCopy;  
 
     assert(oSymTable != NULL);
     assert(pcKey != NULL);
@@ -90,7 +73,7 @@ int SymTable_put(SymTable_T oSymTable,
         return 0;   /* already exists - leave unchanged */
     } 
 
-    new = (struct Node*) malloc(sizeof(struct Node));   /* QUESTION: do i have to cast this? */
+    new = (struct Node*) malloc(sizeof(struct Node)); 
     if (new == NULL) {
         return 0;   /* insufficient memory - leave unchanged */
     }
@@ -98,31 +81,28 @@ int SymTable_put(SymTable_T oSymTable,
     /* make a defensive copy of the key */
     keyCopy = (const char*) malloc((strlen(pcKey)+1)*sizeof(char));
     if (keyCopy == NULL) {
-        return 0;   /* insufficient memory for the key - leave unchanged? QUESTION*/
+        return 0;   /* insufficient memory for the key - unchanged */
     }
     strcpy((char*)keyCopy,pcKey);
 
     /* reorganize links between pointers */
-    new->next = oSymTable->first;    /* if table was empty, new->next is NULL*/
+    new->next = oSymTable->first;  
     oSymTable->first = new;  
 
     /* store address of defensive key copy, and store value */
     new->key = keyCopy;
-    new->item = pvValue;    /* do not dereference bc pvValue is a void pointer */
+    new->item = pvValue;  
 
-    /* NOTE the members are actually POINTERS (so addresses) */
-
+    /* update length */
     (*oSymTable).len++;
 
     return 1;
 
    }
 
-/* If oSymTable contains a binding with key pcKey, then SymTable_replace 
-must replace the binding's value with pvValue and return the old value. 
-Otherwise it must leave oSymTable unchanged and return NULL.*/
+
 void *SymTable_replace(SymTable_T oSymTable,
-   const char *pcKey, const void *pvValue){
+                       const char *pcKey, const void *pvValue){
 
     struct Node *curr;
     const void *old_val;
@@ -132,7 +112,7 @@ void *SymTable_replace(SymTable_T oSymTable,
 
     curr = oSymTable->first;
 
-    /* not using contains bc worst case it traverses twice */
+    /* traverses the list at most 1 time */
     while(curr != NULL) {
         if (strcmp(curr->key, pcKey) == 0) {
             old_val = curr->item;
@@ -147,19 +127,19 @@ void *SymTable_replace(SymTable_T oSymTable,
 
 }
 
-/* return the value of the binding within oSymTable whose key is pcKey, 
-or NULL if no such binding exists*/
+
 void *SymTable_get(SymTable_T oSymTable, const char *pcKey){
-    struct Node *curr;
+                   struct Node *curr;
 
     assert(oSymTable != NULL);
     assert(pcKey != NULL);
 
     curr = oSymTable->first;
 
+    /* traverses the list at most 1 time */
     while(curr != NULL) {
         if (strcmp(curr->key, pcKey) == 0) {
-            return (void*)curr->item;   /* returning a void pointer to value */
+            return (void*)curr->item; 
         }
         curr = curr->next;
     }
@@ -167,8 +147,7 @@ void *SymTable_get(SymTable_T oSymTable, const char *pcKey){
     return NULL;   /* if no matches above */
 }
 
-/* SymTable_contains must return 1 (TRUE) if oSymTable contains a 
-binding whose key is pcKey, and 0 (FALSE) otherwise*/
+
 int SymTable_contains(SymTable_T oSymTable, const char *pcKey){
     struct Node *curr;
 
@@ -177,11 +156,8 @@ int SymTable_contains(SymTable_T oSymTable, const char *pcKey){
 
     curr = oSymTable->first;
 
-    /* assuming that if there is a key, the binding is not null
-    (or counting null values too)*/
     while(curr != NULL) {
-        /* you can dereference the key pointer member (char)*/
-        /* QUESTION: should I match the value (makes sense) or the addresses (that I suppose the client would have to know?)*/
+        /* find matches for the given key */
         if (strcmp(curr->key, pcKey) == 0) {
             return 1;
         }
@@ -189,13 +165,11 @@ int SymTable_contains(SymTable_T oSymTable, const char *pcKey){
         curr = curr->next;
     }
 
-    return 0;   /* returns 0 if empty too*/
+    return 0;  
 
 }
 
-/* If oSymTable contains a binding with key pcKey, then SymTable_remove 
-must remove that binding from oSymTable and return the binding's value. 
-Otherwise the function must not change oSymTable and return NULL.*/
+
 void *SymTable_remove(SymTable_T oSymTable, const char *pcKey){
 
     struct Node *curr, *prev, *next_node;
@@ -205,9 +179,9 @@ void *SymTable_remove(SymTable_T oSymTable, const char *pcKey){
     assert(pcKey != NULL);
 
     curr = oSymTable->first;
-    prev = oSymTable->first;   /* the same at first*/
+    prev = oSymTable->first;   /* initialize at the same point */
 
-    /* not using contains bc worst case it traverses twice */
+    /* traverses the list at most 1 time */
     while(curr != NULL) {
         if (strcmp(curr->key, pcKey) == 0) {
             old_val = curr->item;
@@ -235,23 +209,18 @@ void *SymTable_remove(SymTable_T oSymTable, const char *pcKey){
 }
 
 
-/* apply function *pfApply to each binding (and members) in oSymTable, 
-passing pvExtra as an extra parameter.*/
 void SymTable_map(SymTable_T oSymTable,
    void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
    const void *pvExtra) {
-    /* pfApply is a function pointer. when dereferenced, yields a
-    function that takes 3 params and returns a void pointer*/
 
     struct Node *curr;
 
     assert(oSymTable != NULL);
-    assert(pfApply != NULL);  /* QUESTION: can you do this */
+    assert(pfApply != NULL);
 
     curr = oSymTable->first;
 
     while(curr != NULL) {
-        /* apply the function (not return)*/
         (*pfApply) (curr->key, (void*)curr->item, (void*)pvExtra);
         curr = curr->next;
     }
