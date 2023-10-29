9d6c75a0901d84914dfde7273e4d8b71c62ada35
Alejandra Duran <ad2002@princeton.edu>
2023-10-29 17:53:28 -0400

    debug

--- symtablehash.c
+++ symtablehash.c
@@ -341,6 +341,7 @@ void SymTable_map(SymTable_T oSymTable,
     buckNum = auBucketCounts[oSymTable->iBucket];
 
     for (curr_bucket = oSymTable->buckets[i]; i < buckNum; i++) {
+        printf(curr_bucket);
         while (curr_bucket != NULL) {
             /* apply function */
             printf("I AM IN LOOP\n");
